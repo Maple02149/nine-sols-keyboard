ea13788ae665221a45e4400127ea3de221bbc141
mapleDesktopW\maple <b20060187>
2024-12-07 19:07:50 +0800

    update config setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './build',
-    emptyOutDir: true,
-  },
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({ mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 
+  // `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    build: {
+      outDir: './build',
+      emptyOutDir: true,
+    },
+    base: env.BASE_URL ?? ".",
+    // vite 配置
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  }
 })
