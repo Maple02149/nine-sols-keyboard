a65a42e67832f2a880ce0413cf7decf631cb363e
mapleDesktopW\maple <b20060187>
2024-12-08 02:13:42 +0800

    update keyboard layout and logic

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -7,17 +7,49 @@ interface IProps {
     onChange: (input: string) => void
     keyboardRef: MutableRefObject<any>
 }
+enum ILayoutName {
+    shift = "shift",
+    default = "default"
+}
+enum IBtnStatus {
+    shift = "shift",
+    default = "default",
+    capslock = "capslock"
+}
 function NSF_Keyboard(porps: IProps) {
-    const [layoutName, setLayoutName] = useState("default")
+    const [layoutName, setLayoutName] = useState<ILayoutName>(ILayoutName.default)
+    const [btnStatus, setBtnStatus] = useState<IBtnStatus>(IBtnStatus.default)
 
     const onKeyPress = (button: string) => {
-        if (button === "{capslock}" || button === "{shiftleft}"|| button === "{shiftright}") {
-            setLayoutName(layoutName === "default" ? "shift" : "default")
+        switch (button) {
+            case "{shiftleft}":
+            case "{shiftright}":
+            case "{shift}":
+                if (btnStatus === IBtnStatus.shift) {
+                    setLayoutName(ILayoutName.default)
+                    setBtnStatus(IBtnStatus.default)
+                } else {
+                    setLayoutName(ILayoutName.shift)
+                    setBtnStatus(IBtnStatus.shift)
+                }
+
+                break
+            case "{capslock}":
+                btnStatus === IBtnStatus.capslock ? setBtnStatus(IBtnStatus.default) : setBtnStatus(IBtnStatus.capslock)
+                if (btnStatus === IBtnStatus.capslock) {
+                    setLayoutName(ILayoutName.shift)
+                } else {
+                    setLayoutName(ILayoutName.default)
+                }
+                break
+            default:
+                if (btnStatus === IBtnStatus.shift) {
+                    setLayoutName(ILayoutName.default)
+                }
+                break
         }
     }
 
-
- 
 
 
     return (
@@ -35,32 +67,30 @@ function NSF_Keyboard(porps: IProps) {
                 layout={
                     {
                         default: [
-                            "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
                             "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
                             "{tab} q w e r t y u i o p [ ] \\",
-                            "{capslock} a s d f g h j k l ; ' {enter}",
-                            "{shiftleft} z x c v b n m , . / {shiftright}",
-                            "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+                            "{capslock} a s d f g h j k l ; '",
+                            "{shiftleft} z x c v b n m , . /",
+                            "{space}"
                         ],
                         shift: [
-                            "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
                             "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
                             "{tab} Q W E R T Y U I O P { } |",
-                            '{capslock} A S D F G H J K L : " {enter}',
-                            "{shiftleft} Z X C V B N M < > ? {shiftright}",
-                            "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+                            '{capslock} A S D F G H J K L : "',
+                            "{shiftleft} Z X C V B N M < > ?",
+                            "{space}"
                         ]
                     }
                 }
                 display={
                     {
-                        "{escape}": "esc ⎋",
-                        "{tab}": "tab ⇥",
-                        "{backspace}": "backspace ⌫",
-                        "{enter}": "enter ↵",
-                        "{capslock}": "caps lock ⇪",
-                        "{shiftleft}": "shift ⇧",
-                        "{shiftright}": "shift ⇧",
+                        "{escape}": "⎋",
+                        "{tab}": "⇥",
+                        "{backspace}": "⌫",
+                        "{enter}": "↵",
+                        "{capslock}": "⇪",
+                        "{shiftleft}": "shi⇧",
+                        "{shiftright}": "shi⇧",
                         "{controlleft}": "ctrl ⌃",
                         "{controlright}": "ctrl ⌃",
                         "{altleft}": "alt ⌥",
