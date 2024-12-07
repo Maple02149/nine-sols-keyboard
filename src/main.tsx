cea4355201ba5c23a9dc6fa11fe70160e9bb2a73
mapleDesktopW\maple <b20060187>
2024-12-07 23:33:19 +0800

    add pwa setting (?)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
-
+const intervalMS = 60 * 60 * 1000
+const updateSW = registerSW({
+  onOfflineReady() { },
+  onRegistered(r) {
+    r &&
+      setInterval(() => {
+        r.update()
+      }, intervalMS)
+  },
+  onRegisterError(error) {
+    console.error(new Date(), "onError", JSON.stringify(error))
+  },
+})
+updateSW()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,67 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig, loadEnv } from 'vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ mode }) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 
   // `VITE_` 前缀。
+
+  const maximumFileSize = 6 * 1024 * 1024
+
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [react()],
+    plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      workbox: {
+        sourcemap: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        cleanupOutdatedCaches: false,
+        maximumFileSizeToCacheInBytes: maximumFileSize
+      },
+      manifest: {
+        "name": "Nine Sols Keyboard",
+        "short_name": "Nine Sols Keyboard",
+        "icons": [
+          {
+            "src": "/public/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/public/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/public/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/public/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": "/",
+        "display": "fullscreen",
+        "background_color": "#2E2B2B",
+        "theme_color": "#2E2B2B",
+        "description": "Nine Sols Font Keyboard"
+      }
+
+      // injectManifest: {
+      //   maximumFileSizeToCacheInBytes: maximumFileSize
+      // }
+    })],
     build: {
       outDir: './build',
       emptyOutDir: true,
