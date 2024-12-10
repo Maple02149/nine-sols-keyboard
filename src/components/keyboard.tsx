b10d5c94db4c4c403410eb98986d7e2b8ba113aa
mapleDesktopW\maple <b20060187>
2024-12-10 22:27:11 +0800

    remove tab and space btn

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -68,24 +68,21 @@ function NSF_Keyboard(porps: IProps) {
                     {
                         default: [
                             "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
-                            "{tab} q w e r t y u i o p [ ] \\",
+                            "q w e r t y u i o p [ ] \\",
                             "{capslock} a s d f g h j k l ; '",
                             "{shiftleft} z x c v b n m , . /",
-                            "{space}"
                         ],
                         shift: [
                             "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
-                            "{tab} Q W E R T Y U I O P { } |",
+                            "Q W E R T Y U I O P { } |",
                             '{capslock} A S D F G H J K L : "',
                             "{shiftleft} Z X C V B N M < > ?",
-                            "{space}"
                         ]
                     }
                 }
                 display={
                     {
                         "{escape}": "⎋",
-                        "{tab}": "⇥",
                         "{backspace}": "⌫",
                         "{enter}": "↵",
                         "{capslock}": "⇪",
