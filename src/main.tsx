f98ae9822394217d4e93726af3da304dd09da50c
mapleDesktopW\maple <b20060187>
2024-12-15 13:38:46 +0800

    remove updateSW (as im using    registerType: 'autoUpdate',?)

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App.tsx'
 import './index.css'
-const intervalMS = 60 * 60 * 1000
-const updateSW = registerSW({
-  onOfflineReady() { },
-  onRegistered(r) {
-    r &&
-      setInterval(() => {
-        r.update()
-      }, intervalMS)
-  },
-  onRegisterError(error) {
-    console.error(new Date(), "onError", JSON.stringify(error))
-  },
-})
-updateSW()
+ 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
